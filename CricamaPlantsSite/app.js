4c72451bd18b975336f03a98f780a51043dac309
Carlos Rossi <carlos91rossi@gmail.com>
2021-05-23 12:06:49 -0300

    Comenzando con el Header!

--- CricamaPlantsSite/app.js
+++ CricamaPlantsSite/app.js
@@ -4,8 +4,24 @@ const path = require('path');
 
 app.use(express.static(path.resolve(__dirname,'./public')));
 
-app.listen(3001, () => console.log("Server Start in http://localhost:3001")); 
+app.listen(3000, () => console.log("Server Start in http://localhost:3000")); 
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname,'views','home.html') )
+    res.sendFile(path.join(__dirname,'views','index.html') )
+});
+
+app.get('/register', (req, res) => {
+    res.sendFile(path.join(__dirname,'views','register.html') )
+});
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname,'views','login.html') )
+});
+
+app.get('/productCar', (req, res) => {
+    res.sendFile(path.join(__dirname,'views','productCar.html') )
+});
+
+app.get('/productDetail', (req, res) => {
+    res.sendFile(path.join(__dirname,'views','productDetail.html') )
 });
