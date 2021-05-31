bb98e9223473b70d0fc2f10b62ad225ab54f3b43
Carlos Rossi <carlos91rossi@gmail.com>
2021-05-30 21:54:24 -0300

    Preparacion de archivos para trabajar con Heroku

--- CricamaPlantsSite/app.js
+++ CricamaPlantsSite/app.js
@@ -2,9 +2,11 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(express.static(path.resolve(__dirname,'./public')));
+app.set("port",process.env.PORT || 3000);
 
-app.listen(3000, () => console.log("Server Start in http://localhost:3000")); 
+app.listen(app.get("port"), () => console.log("Server Start in http://localhost:"+app.get("port"))); 
+
+app.use(express.static(path.resolve(__dirname,'./public')));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname,'views','index.html') )
