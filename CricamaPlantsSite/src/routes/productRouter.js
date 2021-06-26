81fc1efaaa4d85365548a38807071a72fe4787af
Carlos Rossi <carlos91rossi@gmail.com>
2021-06-26 16:16:59 -0300

    Creacion de carpeta models

--- CricamaPlantsSite/src/routes/productRouter.js
+++ CricamaPlantsSite/src/routes/productRouter.js
@@ -9,3 +9,18 @@ router.get("/productDetail",productController.productDetail);
 router.get("/productLoad",productController.productLoad);
 
 module.exports = router;
+
+
+/* router.get("/",product.index)
+
+router.get("/create",product.create)
+
+router.get("/:id",product.show)
+
+router.get("/edit/:id",product.edit)
+
+router.post("/save",[upload.single("image")],product.save)
+
+router.put("/update/:id",[upload.single("image")],product.update)
+
+router.delete("/delete/:id",product.delete) */
