843bb912d34b6901d51a15c0fe0e66c9540976e1
CarlosARossi <carlos91rossi@gmail.com>
2021-07-13 00:16:36 -0300

    Agregado de primera parte de express-validator

--- CricamaPlantsSite/src/routes/userRouter.js
+++ CricamaPlantsSite/src/routes/userRouter.js
@@ -1,8 +1,10 @@
-let express = require('express')
+//Requieres
+let express = require('express');
 let router = express.Router();
 const userController = require('../controllers/userController');
 const path = require('path');
-const multer = require ("multer")
+const multer = require ('multer');
+const { body } = require ('express-validator');
 
 //Route for images
 let dest = multer.diskStorage({
@@ -18,28 +20,30 @@ let dest = multer.diskStorage({
 })
 const upload = multer({storage:dest});
 
-//Routes
+const validations = [
+    body('firstName').notEmpty().withMessage('Tenes que escribir tu nombre'),
+    body('lastName').notEmpty().withMessage('Tenes que escribir tu apellido'),
+    body('email')
+        .notEmpty().withMessage('Tenes que escribir tu email').bail()
+        .isEmail().withMessage('Formato de mail inválido'),
+    body('password').notEmpty().withMessage('Tenes que escribir una contraseña'),
+]
 
+//Routes
 //Login User
 router.get("/login",userController.login);
-
 //User Profile
 router.get("/userProfile/:id",userController.userProfile);
-
 //List of all users
 router.get("/users/:id?",userController.list);
-
-//Create user
+//Register a new user
 router.get("/register",userController.registerForm);
-router.post("/register",[upload.single("image")],userController.register);
-
+router.post("/register",[upload.single("image")],validations,userController.register);
 //Edit user
 router.get("/userEdit/:id",userController.userEdit);
 router.put("/userSave/:id",[upload.single("image")],userController.userSave);
-
 //Delete user
 router.delete("/userDelete/:id",userController.userDelete)
-
 //Admin Profile
 router.get("/admin",userController.admin);
 
