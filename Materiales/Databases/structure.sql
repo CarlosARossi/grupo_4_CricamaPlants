567fe2ca20ccfde5499fd69c236006dbabb4e363
CarlosARossi <carlos91rossi@gmail.com>
2021-08-09 15:36:38 -0300

    Reubicacion de archivo io y agregado de .DS_Store en gitignore

--- Materiales/Databases/structure.sql
+++ Materiales/Databases/structure.sql
@@ -2,10 +2,10 @@ CREATE DATABASE cricama;
 
 CREATE TABLE users (
 	id AUTO_INCREMENT,
-  	created_at TIMESTAMP,
-  	updated_at TIMESTAMP,
-  	first_name VARCHAR,
-  	last_name VARCHAR,
+	created_at TIMESTAMP,
+	updated_at TIMESTAMP,
+	first_name VARCHAR,
+	last_name VARCHAR,
     email VARCHAR,
     password VARCHAR,
     image VARCHAR,
@@ -16,12 +16,12 @@ CREATE TABLE users (
 
 CREATE TABLE products (
 	id AUTO_INCREMENT,
-  	created_at TIMESTAMP,
-  	updated_at TIMESTAMP,
+	created_at TIMESTAMP,
+	updated_at TIMESTAMP,
     name VARCHAR,
     description TEXT,
     image VARCHAR,
-  	price INTEGER,
+	price INTEGER,
 	category_id INTEGER,
 	PRIMARY KEY (id),
 	FOREIGN KEY (category_id) REFERENCES categories (id)
@@ -29,12 +29,12 @@ CREATE TABLE products (
 
 CREATE TABLE users_products (
 	id AUTO_INCREMENT,
-  	created_at TIMESTAMP,
-  	updated_at TIMESTAMP,
+	created_at TIMESTAMP,
+	updated_at TIMESTAMP,
     user_id INTEGER,
     product_id INTEGER,
-  	quantity INTEGER,
-  	price INTEGER,
+	quantity INTEGER,
+	price INTEGER,
 	PRIMARY KEY (id),
 	FOREIGN KEY (user_id) REFERENCES users (id),
 	FOREIGN KEY (product_id) REFERENCES products (id) 
@@ -42,16 +42,16 @@ CREATE TABLE users_products (
 
 CREATE TABLE users_types (
 	id AUTO_INCREMENT,
-  	created_at TIMESTAMP,
-  	updated_at TIMESTAMP,
-  	type VARCHAR,
+	created_at TIMESTAMP,
+	updated_at TIMESTAMP,
+	type VARCHAR,
 	PRIMARY KEY (id)
 )
 
 CREATE TABLE categories (
 	id AUTO_INCREMENT,
-  	created_at TIMESTAMP,
-  	updated_at TIMESTAMP,
-  	category VARCHAR,
+	created_at TIMESTAMP,
+	updated_at TIMESTAMP,
+	category VARCHAR,
 	PRIMARY KEY (id)
 )
