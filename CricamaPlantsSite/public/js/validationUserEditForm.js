75cb34cb18e7338ad482cb360561c7709860ebb3
manunacht <manivanac@gmail.com>
2021-09-05 10:47:10 -0300

    animacion de errores y validacion de edicion de usuarios a revisar

--- CricamaPlantsSite/public/js/validationUserEditForm.js
+++ CricamaPlantsSite/public/js/validationUserEditForm.js
@@ -10,7 +10,6 @@ window.addEventListener('load', function(){
         let firstName = document.querySelector("input[name='firstName']");
         let lastName = document.querySelector("input[name='lastName']");
         let email = document.querySelector("input[name='email']");
-        let image = document.querySelector("input[name='image']");
         let password = document.querySelector("input[name='password']");
         let passwordConfirm = document.querySelector("input[name='passwordConfirm']");
 
@@ -23,9 +22,6 @@ window.addEventListener('load', function(){
         if(email.value == ""){
             errors.push("El campo email no debe estar vacío");
         }
-        if(image.value == ""){
-            errors.push("Agrega una imagen");
-        }
         if(password.value == ""){
             errors.push("Te falta una contraseña");
         }
@@ -37,7 +33,7 @@ window.addEventListener('load', function(){
             let ulErrors = document.querySelector('div.errors ul');
             ulErrors.innerHTML = '';
             for(let i = 0; i < errors.length; i++){
-                ulErrors.innerHTML += "<li>"+ errors[i] + "</li>"
+                ulErrors.innerHTML += "<li>" + errors[i] + "</li>"
             }
             /*  errors.forEach(error => {
                 ulErrors.innerHTML += `<li>${error}</li>`
