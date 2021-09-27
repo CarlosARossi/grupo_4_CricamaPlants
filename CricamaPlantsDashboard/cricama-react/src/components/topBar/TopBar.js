fb8ce042ef145c605602d01401f51ca0619f7588
CristianGus <cristian.leivalot@gmail.com>
2021-09-27 19:13:28 -0300

    dashboard

---
+++ CricamaPlantsDashboard/cricama-react/src/components/topBar/TopBar.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import TopInfo from './TopInfo';
+import PropTypes from 'prop-types';
+
+function TopBar(props) {
+    return(
+    
+        <React.Fragment>
+
+            <TopInfo title="Total de productos en stock: " total=""/>
+            <TopInfo title="Total de usuarios: " total=""/>
+            <TopInfo title="Total de categorías: " total=""/>
+
+        </React.Fragment>)
+
+}
+
+TopBar.propTypes = {
+    title: PropTypes.string,
+    total: PropTypes.number
+}
+
+TopBar.defaultProps = {
+    title: "A definir",
+    total: 0
+}
+
+export default TopBar;
