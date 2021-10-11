83d099c56824517f3a535769949ae68b0201ed3c
CarlosARossi <carlos91rossi@gmail.com>
2021-10-11 12:09:33 -0300

    Correccion en config database

--- CricamaPlantsSite/src/database/config/config.js
+++ CricamaPlantsSite/src/database/config/config.js
@@ -8,17 +8,19 @@ module.exports = {
     "dialect": "mysql"
   },
   "test": {
-    "username": "root",
-    //"password": "root",
-    "database": "cricama_db",
-    "host": "127.0.0.1",
+    "username": "ufyba6y6tjztm1u2",
+    "password": "IrT22IdIc3fAXvPpS0lw",
+    "database": "bi3jkh7pguoxj0jqwup0",
+    "host": "bi3jkh7pguoxj0jqwup0-mysql.services.clever-cloud.com",
+    "port": "3306",
     "dialect": "mysql"
   },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
+    "username": "ufyba6y6tjztm1u2",
+    "password": "IrT22IdIc3fAXvPpS0lw",
+    "database": "bi3jkh7pguoxj0jqwup0",
+    "host": "bi3jkh7pguoxj0jqwup0-mysql.services.clever-cloud.com",
+    "port": "3306",
     "dialect": "mysql"
   }
 }
